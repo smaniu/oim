2771417b9c69b1f3664eb31806e9dc74a9935d95
plagree <paul.lagree@gmail.com>
2017-02-08 11:59:30 +0100

    Debugging

--- Strategy.hpp
+++ Strategy.hpp
@@ -231,15 +231,14 @@ class MissingMassStrategy : public Strategy {
       total_spread.insert(spread.begin(), spread.end());
 
       // 3. (c) Update statistics of experts
-      std::vector<unode_int> expert_nodes;  // For each expert, the associated node in the graph
-      std::cerr << "Update statistics of experts" << std::endl;
-      for (unsigned int chosen_expert : chosen_experts)
-        expert_nodes.push_back(experts[chosen_expert]);
-      auto expert_spreads = extract_expert_spreads(
-            spread, expert_nodes, k);  // Spread associated to each expert
+      // std::vector<unode_int> expert_nodes;  // For each expert, the associated node in the graph
+      // for (unsigned int chosen_expert : chosen_experts)
+      //   expert_nodes.push_back(experts[chosen_expert]);
+      // auto expert_spreads = extract_expert_spreads(
+      //       spread, expert_nodes, k);  // Spread associated to each expert
       int n_expert = 0;
       for (unsigned int expert : chosen_experts) {
-        policy->updateState(expert, expert_spreads[n_expert]);
+        policy->updateState(expert, spread/*expert_spreads[n_expert]*/);
         n_expert++;
       }
       t2 = get_timestamp();
@@ -247,8 +246,6 @@ class MissingMassStrategy : public Strategy {
       roundtime = (double)(t2 - t0) / 1000000;
       totaltime += roundtime;
       // memory = disp_mem_usage();
-      std::cerr << "Diffusion " << stage << " done" << std::endl;
-      std::cerr << std::flush;
 
       // 4. Printing results
       std::cout << stage << "\t" << total_spread.size() << '\t'
